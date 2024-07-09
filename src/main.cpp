be95ddef91b1fc81f79c77eab05dcb9bf8730b1d
M. Haris Mujeeb <57053470+haris-mujeeb@users.noreply.github.com>
2024-07-09 19:53:46 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,13 @@
 #include "ciphers.hpp"
 
 int main(int argc, char* argv[]) {
-    // ciphers::Morse myCipherEngine; 
-    ciphers::Caeser myCipherEngine; 
+
     try {
-        // if (argc < 2) {
-        //     std::cerr << "Error: Please provide an executable file argument." << std::endl;
-        // return 1;
-        // }
-        // else {
+        if (argc < 2) {
+            std::cerr << "Error: Please provide an executable file argument." << std::endl;
+        return 1;
+        }
+        else {
         
             // std::string  text = "Hello Easy C++ project!";
             // std::string  encoded = myCipherEngine.encoder(text);
@@ -25,21 +24,51 @@ int main(int argc, char* argv[]) {
             // std::cout << encoded << std::endl;
             // std::cout << decoded << std::endl;
 
-            std::string inputfileName = "testData.txt";
-            std::string outputFileName = "encodedData.txt";
-            // std::string inputfileName = argv[1];
-            // std::string outputFileName = argv[2];
+            // std::string inputfileName = "testData.txt";
+            // std::string outputFileName = "encodedData.txt";
+            std::string inputfileName;
+            std::string outputFileName;
+                    
+            if (argv[1] == "morse") {
+                ciphers::Morse myCipherEngine;
+                inputfileName = argv[3];
+                outputFileName = argv[4];                
+                if (argv[2] == "encode") {
+                    std::string text = myCipherEngine.readFromFile(inputfileName);
+                    std::string encoded = myCipherEngine.encoder(text);
+                    myCipherEngine.writeToFile(encoded, outputFileName);
+                } else if (argv[2] == "decode") {
+                    std::string text = myCipherEngine.readFromFile(inputfileName);
+                    std::string decoded = myCipherEngine.decoder(text);
+                    myCipherEngine.writeToFile(decoded, outputFileName);
+                }
+            } else if (argv[1] == "caeser") {
+                ciphers::Caeser myCipherEngine;
+                inputfileName = argv[4];
+                outputFileName = argv[5];
+                int shiftBy = std::stoi(argv[2]);
+                if (argv[2] == "encode") {
+                    std::string text = myCipherEngine.readFromFile(inputfileName);
+                    std::string encoded = myCipherEngine.encoder(text, shiftBy);
+                    myCipherEngine.writeToFile(encoded, outputFileName);
+                } else if (argv[2] == "decode") {
+                    std::string text = myCipherEngine.readFromFile(inputfileName);
+                    std::string decoded = myCipherEngine.decoder(text, 5);
+                    myCipherEngine.writeToFile(decoded, outputFileName);
+                }
+            }
 
-            std::string text = myCipherEngine.readFromFile(inputfileName);
-            // std::string encoded = myCipherEngine.encoder(text);
-            std::string encoded = myCipherEngine.encoder(text, 5);
-            myCipherEngine.writeToFile(encoded, outputFileName);
 
-            std::string encodedText = myCipherEngine.readFromFile(outputFileName);
-            // std::string  decodedText = myCipherEngine.decoder(encodedText); 
-            std::string  decodedText = myCipherEngine.decoder(encodedText, 5); 
-            myCipherEngine.writeToFile(decodedText, "decodedData.txt");
-        // }
+        //     std::string text = myCipherEngine.readFromFile(inputfileName);
+        //     // std::string encoded = myCipherEngine.encoder(text);
+        //     std::string encoded = myCipherEngine.encoder(text, 5);
+        //     myCipherEngine.writeToFile(encoded, outputFileName);
+
+        //     std::string encodedText = myCipherEngine.readFromFile(outputFileName);
+        //     // std::string  decodedText = myCipherEngine.decoder(encodedText); 
+        //     std::string  decodedText = myCipherEngine.decoder(encodedText, 5); 
+        //     myCipherEngine.writeToFile(decodedText, "decodedData.txt");
+        }
     } catch (const std::runtime_error& error) {
         std::cerr << error.what() << std::endl; 
     }
