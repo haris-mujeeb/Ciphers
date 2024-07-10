bfe148d39a129c23a4c06329dd2148606bebf4a9
M. Haris Mujeeb <57053470+haris-mujeeb@users.noreply.github.com>
2024-07-10 13:04:04 +0200

    Update main.cpp
    added more functionalities

--- src/main.cpp
+++ src/main.cpp
@@ -8,68 +8,82 @@
 #include <iostream>
 #include "ciphers.hpp"
 
+void processMorseCipher(const std::string& action, const std::string& inputfileName, const std::string& outputFileName);
+void processCaeserCipher(const std::string& action, const std::string& shiftBy, const std::string& inputfileName, const std::string& outputFileName);
 int main(int argc, char* argv[]) {
-
     try {
-        if (argc < 2) {
-            std::cerr << "Error: Please provide an executable file argument." << std::endl;
-        return 1;
+        if (argc < 3) {
+            std::cerr << "Usage: " << argv[0] << " <cipher> <action> [shift] <inputfile> <outputfile> " << std::endl;
+            return 1;
         }
         else {
-        
-            // std::string  text = "Hello Easy C++ project!";
-            // std::string  encoded = myCipherEngine.encoder(text);
-            // std::string  decoded = myCipherEngine.decoder(encoded); 
-            // std::cout << text << std::endl;
-            // std::cout << encoded << std::endl;
-            // std::cout << decoded << std::endl;
-
-            // std::string inputfileName = "testData.txt";
-            // std::string outputFileName = "encodedData.txt";
-            std::string inputfileName;
-            std::string outputFileName;
-                    
-            if (argv[1] == "morse") {
-                ciphers::Morse myCipherEngine;
-                inputfileName = argv[3];
-                outputFileName = argv[4];                
-                if (argv[2] == "encode") {
-                    std::string text = myCipherEngine.readFromFile(inputfileName);
-                    std::string encoded = myCipherEngine.encoder(text);
-                    myCipherEngine.writeToFile(encoded, outputFileName);
-                } else if (argv[2] == "decode") {
-                    std::string text = myCipherEngine.readFromFile(inputfileName);
-                    std::string decoded = myCipherEngine.decoder(text);
-                    myCipherEngine.writeToFile(decoded, outputFileName);
-                }
-            } else if (argv[1] == "caeser") {
-                ciphers::Caeser myCipherEngine;
-                inputfileName = argv[4];
-                outputFileName = argv[5];
-                int shiftBy = std::stoi(argv[2]);
-                if (argv[2] == "encode") {
-                    std::string text = myCipherEngine.readFromFile(inputfileName);
-                    std::string encoded = myCipherEngine.encoder(text, shiftBy);
-                    myCipherEngine.writeToFile(encoded, outputFileName);
-                } else if (argv[2] == "decode") {
-                    std::string text = myCipherEngine.readFromFile(inputfileName);
-                    std::string decoded = myCipherEngine.decoder(text, 5);
-                    myCipherEngine.writeToFile(decoded, outputFileName);
-                }
+            std::string cipherType = argv[1];
+            if ( cipherType == "morse") {
+                if (argc == 4 ) {
+                    std::string defaultName = "output.txt";
+                    std::cout << "Warning: No output file name give. Using default output file name as 'output.txt'" << std::endl;
+                    processMorseCipher ( argv[2], argv[3], defaultName );
+                } else if (argc == 5) processMorseCipher ( argv[2], argv[3], argv[4] );
+                else throw std::runtime_error("Error: too many arguments");
             }
-
-
-        //     std::string text = myCipherEngine.readFromFile(inputfileName);
-        //     // std::string encoded = myCipherEngine.encoder(text);
-        //     std::string encoded = myCipherEngine.encoder(text, 5);
-        //     myCipherEngine.writeToFile(encoded, outputFileName);
-
-        //     std::string encodedText = myCipherEngine.readFromFile(outputFileName);
-        //     // std::string  decodedText = myCipherEngine.decoder(encodedText); 
-        //     std::string  decodedText = myCipherEngine.decoder(encodedText, 5); 
-        //     myCipherEngine.writeToFile(decodedText, "decodedData.txt");
+            else if ( cipherType == "caeser" ) {
+                if ( argc == 5 ) {
+                    std::string defaultName = "output.txt";
+                    std::cout << "Warning: No output file name give. Using default output file name as 'output.txt'" << std::endl;
+                    processCaeserCipher ( argv[2], argv[3], argv[4], defaultName );
+                } else if ( argc == 6) processCaeserCipher ( argv[2], argv[3], argv[4], argv[5] );
+                else throw std::runtime_error("Error: too many arguments");
+            } 
+            else {
+                std::cerr << "Error: Unknown cipher type '" << cipherType << "'." << std::endl;
+                throw std::runtime_error("Invalid action for caesar cipher");
+            }
         }
     } catch (const std::runtime_error& error) {
         std::cerr << error.what() << std::endl; 
+        return 1;
+    } 
+    return 0;
+}
+
+
+void processMorseCipher(const std::string& action, const std::string& inputfileName, const std::string& outputFileName) {    
+    ciphers::Morse myCipherEngine;              
+    if (action == "encode") {
+        std::string text = myCipherEngine.readFromFile(inputfileName);
+        std::string encoded = myCipherEngine.encoder(text);
+        myCipherEngine.writeToFile(encoded, outputFileName);
+    } else if (action == "decode") {
+        std::string text = myCipherEngine.readFromFile(inputfileName);
+        std::string decoded = myCipherEngine.decoder(text);
+        myCipherEngine.writeToFile(decoded, outputFileName);
+    } else {
+        std::cerr << "Error: Unknown action '" << action << "' for morse cipher." << std::endl;
+        throw std::runtime_error("Invalid action for morse cipher");
+    }
+}
+
+
+void processCaeserCipher(const std::string& action, const std::string& shift, const std::string& inputfileName, const std::string& outputFileName) {    
+    int shiftBy;
+    try {
+        shiftBy = std::stoi(shift);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: Invalid shift value." << std::endl;
+        throw std::runtime_error("Invalid shift value for caesar cipher");
+    }
+    
+    ciphers::Caeser myCipherEngine;
+    if (action == "encode") {
+        std::string text = myCipherEngine.readFromFile(inputfileName);
+        std::string encoded = myCipherEngine.encoder(text, shiftBy);
+        myCipherEngine.writeToFile(encoded, outputFileName);
+    } else if (action == "decode") {
+        std::string text = myCipherEngine.readFromFile(inputfileName);
+        std::string decoded = myCipherEngine.decoder(text, shiftBy);
+        myCipherEngine.writeToFile(decoded, outputFileName);
+    } else {
+        std::cerr << "Error: Unknown action '" << action << "' for caeser cipher." << std::endl;
+        throw std::runtime_error("Invalid action for caesar cipher");
     }
 }
