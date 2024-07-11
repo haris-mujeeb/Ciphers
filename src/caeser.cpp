11c4ca4841089a059fba009767670921c101ce2d
haider-rizvi-github <serveronthego1@gmail.com>
2024-07-11 19:12:37 +0200

    Update caeser.cpp

--- src/caeser.cpp
+++ src/caeser.cpp
@@ -13,47 +13,47 @@
 
 std::string ciphers::Caeser::encoder( const std::string input_message, int shiftBy) {
     std::string encoded_message;
+    std::string encoded_message;
     unsigned char eholder;
     std::string Fencoded;
     for (unsigned char ch : input_message) {
         if(int(ch) <= 32 && int(ch) >= 126)
             throw std::runtime_error( "Restricted character used in text!");
         if (ch != ' ') {
-            eholder = ch; // Convert to uppercase
-            eholder = char(int(ch) + shiftBy);
-        } else eholder = ' ';
+            eholder = std::toupper(ch); // Convert to uppercase
+            eholder = char(int(eholder) + shiftBy);
+        } else  {eholder = ' ';}
         encoded_message.push_back(eholder);
     }
-    for(unsigned char ch : encoded_message){
+     for(unsigned char ch : encoded_message){
         if (ch != ' ') {
             eholder= std::toupper(ch);
-        } else eholder = ' ';
-        Fencoded.push_back(eholder);
-    }
+            Fencoded.push_back(eholder);
+        }
+        }
     return encoded_message;
 }
 
 
 std::string ciphers::Caeser::decoder( const std::string istring, int shiftBy) {     
-    std::string decoded;
-    char dholder;
-    std::string Fdecoded; 
-    for (size_t i = 0; i < istring.size(); i++) {
-        char ch = istring[i];
-        if(int(ch) <= 32 && int(ch) >= 126)
-            throw std::runtime_error( "Restricted character used in text!");
-        if (ch != ' ') {
-            // dholder = std::toupper(ch); // Convert to uppercase
-            dholder = char(int(ch) - shiftBy);
-        } else dholder = ' ';
-        decoded.push_back(dholder);
-    }
-    for (size_t i = 0; i < istring.size(); i++){
-        char ch = istring[i];
-        if (ch != ' ') {
+        std::string decoded;
+        char dholder;
+        std::string Fdecoded;
+        for (unsigned char ch : istring) {
+            if(int(ch) <= 32 && int(ch) >= 126)
+                throw std::runtime_error( "Restricted character used in text!");
+            if (ch != ' ') {
+                dholder = char(int(ch) - shiftBy);
+            } else {dholder = ' ';}
+            decoded.push_back(dholder);
+        }
+        for(char ch : decoded){
+             if (ch != ' ') {
             dholder = std::toupper(ch);
-        } else dholder = ' ';
-        Fdecoded.push_back(dholder);
+            } else {dholder = ' ';}
+            Fdecoded.push_back(dholder);
+        }
+        return Fdecoded;
     }
     return Fdecoded;
 }
